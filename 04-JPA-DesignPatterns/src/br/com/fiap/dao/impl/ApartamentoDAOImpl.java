9da4dee0cd50885e5bd4d07bf3dd6d792e06dad2
logonrm <logonrm@fiap.com.br>
2017-02-21 20:07:56 -0300

    Create feito

--- 04-JPA-DesignPatterns/src/br/com/fiap/dao/impl/ApartamentoDAOImpl.java
+++ 04-JPA-DesignPatterns/src/br/com/fiap/dao/impl/ApartamentoDAOImpl.java
@@ -1,14 +1,29 @@
 package br.com.fiap.dao.impl;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import br.com.fiap.dao.ApartamentoDAO;
 import br.com.fiap.entity.Apartamento;
 
-public class ApartamentoDAOImpl implements ApartamentoDAO{
+public class ApartamentoDAOImpl implements ApartamentoDAO {
 
 	@Override
 	public void create(Apartamento ap) {
-		// TODO Auto-generated method stub
-		
+		// Obter uma instancia da Fabrica
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("CLIENTE_ORACLE");
+		// Obter uma instancia do EntityManager
+		EntityManager em = fabrica.createEntityManager();
+		// Cadastrar apartamento
+		em.persist(ap);
+		// Commit
+		em.getTransaction().begin();
+		em.getTransaction().commit();
+		// Fechando conexão
+		em.close();
+		System.out.println("Apartamento cadastrado!");
+		System.exit(0);
 	}
 
 	@Override
@@ -20,12 +35,12 @@ public class ApartamentoDAOImpl implements ApartamentoDAO{
 	@Override
 	public void update(Apartamento ap) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete(int id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
