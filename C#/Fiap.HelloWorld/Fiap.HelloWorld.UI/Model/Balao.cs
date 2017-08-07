32b6d5716bd7dba633ea1b72d61dc4ef954681b4
logonrm <logonrm@fiap.com.br>
2017-08-07 20:01:22 -0300

    C#

---
+++ C#/Fiap.HelloWorld/Fiap.HelloWorld.UI/Model/Balao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.HelloWorld.UI.Model
+{
+    class Balao : Veiculo, IVoador
+    {
+        //Construtor
+        public Balao(string placa) : base(placa)
+        {
+        }
+
+        //Metodos
+        public override void Acelerar()
+        {
+            Console.WriteLine("Balao Acelerando");
+        }
+
+        public void Voar()
+        {
+            Console.WriteLine("Balao Voando");
+        }
+    }
+}
---
+++ C#/Fiap.HelloWorld/Fiap.HelloWorld.UI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiap.HelloWorld.UI.Model;
+
+namespace Fiap.HelloWorld.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar um Carro
+            Carro carro = new Carro("FFF-45587");
+            Moto moto = new Moto("FSG-8899")
+            {
+                Bau = true
+            };
+
+            //Criar uma lista de balao
+            var lista = new List<Balao>();
+            lista.Add(new Balao("Mágico"));
+            lista.Add(new Balao("Não Mágico"));
+
+            //E exibir os dados do balao
+            foreach(var item in lista)
+            {
+                Console.WriteLine(item.Placa);
+            }
+            //Atribuir valores a placa e portas
+            carro.Placa = "FFF-44546";
+            carro.Porta = 5;
+            //Exibir a placa
+            Console.WriteLine(carro.Placa);
+            Console.ReadLine();
+        }
+    }
+}
