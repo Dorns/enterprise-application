2ae13ea22214d431c84e3715f422f9c020575a76
Felipe Dornelas Viel <RM76093@fiap.com.br>
2017-08-16 22:44:54 -0300

    Exercicio MVC

---
+++ C#/Fiap.Exercicio01.MVC/Fiap.Exercicio01.MVC/Controllers/CarroController.cs
@@ -0,0 +1,36 @@
+﻿using Fiap.Exercicio01.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exercicio01.MVC.Controllers
+{
+    public class CarroController : Controller
+    {
+        //Simular o banco
+        private static IList<Carro> _lista = new List<Carro>();
+
+        [HttpGet] //Abre o formulario
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]//Processa o formulario
+        public ActionResult Cadastrar(Carro carro)
+        {
+            _lista.Add(carro);
+            TempData["msg"] = "Cadastrado!";
+            return View();
+        }
+
+        //mvcaction4
+        public ActionResult Listar()
+        {
+            //Passa a lista para a tela
+            return View(_lista);
+        }
+    }
+}
---
+++ C#/Fiap.Exercicio01.MVC/Fiap.Exercicio01.MVC/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using Fiap.Exercicio01.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exercicio01.MVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Gravar(Usuario usuario)
+        {
+            var msgRetorno = "";
+
+            try
+            {
+                msgRetorno = "Nome: " + usuario.Nome + "\nIdade: " + usuario.Idade + "\nEmail: " + usuario.Email;
+
+            }
+            catch (Exception ex)
+            {
+                msgRetorno = "Erro: " + ex.Message;
+            }
+
+            TempData["mensagem"] = msgRetorno;
+
+            return View(usuario);
+        }
+    }
+}
