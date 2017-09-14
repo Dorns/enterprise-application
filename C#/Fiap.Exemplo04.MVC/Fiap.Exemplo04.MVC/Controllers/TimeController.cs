81054893a7fbf4d30acf658d12d7c89a492b1455
logonrm <logonrm@fiap.com.br>
2017-09-13 22:31:44 -0300

    Relacionamentos MVC C#

---
+++ C#/Fiap.Exemplo04.MVC/Fiap.Exemplo04.MVC/Controllers/TimeController.cs
@@ -0,0 +1,35 @@
+﻿using Fiap.Exemplo04.MVC.Models;
+using Fiap.Exemplo04.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo04.MVC.Controllers
+{
+    public class TimeController : Controller
+    {
+        private FutebolContext _context = new FutebolContext();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(Time time)
+        {
+            _context.Times.Add(time);
+            _context.SaveChanges();
+            TempData["msg"] = "Cadastrado!";
+            return RedirectToAction("Cadastrar");
+        }
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            //Lista tudo de Times e inclui Tecnico
+            return View(_context.Times.Include("Tecnico").ToList());
+        }
+    }
+}
